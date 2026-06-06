c9aabfe0fd12408d6663a414a7b4d528ec3d2c08
Sebastien Vanlede <sebastien@usepowerhouse.io>
2026-06-06 19:56:28 +0200

    Feat: localStorage persistentie via Zustand persist

--- lib/store.ts
+++ lib/store.ts
@@ -1,5 +1,6 @@
 'use client';
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 import { Formation, PickedPlayer, SimulatedSeason } from './types';
 
 interface GameState {
@@ -18,29 +19,44 @@ interface GameState {
   reset: () => void;
 }
 
-export const useGameStore = create<GameState>((set) => ({
-  formation: null,
-  pickedPlayers: [],
-  currentPositionIndex: 0,
-  simulatedSeason: null,
-  teamName: 'Mijn Droomelftal',
-  draftMode: 'normal',
+export const useGameStore = create<GameState>()(
+  persist(
+    (set) => ({
+      formation: null,
+      pickedPlayers: [],
+      currentPositionIndex: 0,
+      simulatedSeason: null,
+      teamName: 'Mijn Droomelftal',
+      draftMode: 'normal',
 
-  setFormation: (formation) =>
-    set({ formation, pickedPlayers: [], currentPositionIndex: 0, simulatedSeason: null }),
+      setFormation: (formation) =>
+        set({ formation, pickedPlayers: [], currentPositionIndex: 0, simulatedSeason: null }),
 
-  pickPlayer: (player) =>
-    set((state) => ({
-      pickedPlayers: [...state.pickedPlayers, player],
-      currentPositionIndex: state.currentPositionIndex + 1,
-    })),
+      pickPlayer: (player) =>
+        set((state) => ({
+          pickedPlayers: [...state.pickedPlayers, player],
+          currentPositionIndex: state.currentPositionIndex + 1,
+        })),
 
-  setSimulatedSeason: (simulatedSeason) => set({ simulatedSeason }),
+      setSimulatedSeason: (simulatedSeason) => set({ simulatedSeason }),
 
-  setTeamName: (teamName) => set({ teamName }),
+      setTeamName: (teamName) => set({ teamName }),
 
-  setDraftMode: (draftMode) => set({ draftMode }),
+      setDraftMode: (draftMode) => set({ draftMode }),
 
-  reset: () =>
-    set({ formation: null, pickedPlayers: [], currentPositionIndex: 0, simulatedSeason: null }),
-}));
+      reset: () =>
+        set({ formation: null, pickedPlayers: [], currentPositionIndex: 0, simulatedSeason: null }),
+    }),
+    {
+      name: 'pintjesliga-state',
+      partialize: (state) => ({
+        formation:        state.formation,
+        pickedPlayers:    state.pickedPlayers,
+        currentPositionIndex: state.currentPositionIndex,
+        simulatedSeason:  state.simulatedSeason,
+        teamName:         state.teamName,
+        draftMode:        state.draftMode,
+      }),
+    }
+  )
+);
